6cd1c83b68477b0da818dd30778eb685c4d6e014
Leric Zhang <leric.zhang@gmail.com>
2025-06-08 00:16:24 +0800

    Transform Astro blog to Medium-style design

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Thoughtful';
+export const SITE_DESCRIPTION = 'A place for meaningful stories and thoughtful conversations.';
